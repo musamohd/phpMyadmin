9d8031f758bd178ae20204c76d03af8ae4a016a0
musamohd <musamohd89@gmail.com>
2013-02-24 23:58:43 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "mx1.serversfree.com");
+define("DB_USER", "u171100496_login");
+define("DB_PASSWORD", " 59888993");
+define("DB_DATABASE", "u171100496_users");
+?>
